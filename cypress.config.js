3490fe4b8eefd0a6f4db27d8c9b72e6bbd28f7d3
EEYUN <tehpeilo@gmail.com>
2026-02-03 00:13:02 +0100

    chore: update cypress viewport dimensions

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    viewportWidth: 1280,
-    viewportHeight: 720,
+    setupNodeEvents(on, config) {},
+    viewportWidth: 1920,
+    viewportHeight: 1080,
   },
 });
